123c803a74b9101c87765d9790623218bde0276e
iver56 <ij@aiascience.com>
2019-08-14 15:10:30 +0200

    Add convenience functions for storing and retrieving datasets

--- README.rst
+++ README.rst
@@ -23,10 +23,21 @@ Installation
 
 Usage
 
+.. code:: python
+
     import mflux_ai
 
     mflux_ai.set_env_vars(token="insert_your_token_here")
 
+Storing and retrieving datasets
 
+.. code:: python
 
+    my_dataset = np.zeros(shape=(10000, 100), dtype=np.float32)
+    dataset_filename = "my-dataset.pkl"
 
+    mflux_ai.put_dataset(my_dataset, dataset_filename)
+
+    my_loaded_dataset = mflux_ai.get_dataset(dataset_filename)
+
+    assert_array_equal(my_dataset, my_loaded_dataset)
--- setup.py
+++ setup.py
@@ -5,42 +5,42 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['requests', ]
+requirements = ["joblib<1", "minio>=4,<5", "requests"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = ["pytest"]
 
 setup(
     author="AIA Science AS",
-    author_email='mflux.ai@aiascience.com',
+    author_email="mflux.ai@aiascience.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Open source code for the mflux-ai python package",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='mflux_ai',
-    name='mflux-ai',
-    packages=find_packages(include=['mflux_ai']),
+    keywords="mflux_ai",
+    name="mflux-ai",
+    packages=find_packages(include=["mflux_ai"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
     url='https://github.com/AIAScience/mflux-ai-python',
     version='0.1.1',
