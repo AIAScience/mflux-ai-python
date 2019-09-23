cb082cb9f042e2e99a1aea3bb484a0aa8538539d
iver56 <ij@aiascience.com>
2019-09-23 10:34:06 +0200

    Transition from Alpha to Beta

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     author="AIA Science AS",
     author_email="mflux.ai@aiascience.com",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
